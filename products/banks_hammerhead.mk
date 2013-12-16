5149b2f902712f78a700830bc564c0ac9782aee6
MrBaNkS <banksmi09@gmail.com>
2013-12-15 21:31:58 -0600

    Update Hammerhead fingerprint 4.4.2

--- products/banks_hammerhead.mk
+++ products/banks_hammerhead.mk
@@ -11,5 +11,5 @@ PRODUCT_DEVICE := hammerhead
 PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT="google/hammerhead/hammerhead:4.4.1/KOT49E/926649:user/release-keys" PRIVATE_BUILD_DESC="hammerhead-user 4.4.1 KOT49E 926649 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT="google/hammerhead/hammerhead:4.4.2/KOT49H/937116:user/release-keys" PRIVATE_BUILD_DESC="hammerhead-user 4.4.2 KOT49H 937116 release-keys"
 
